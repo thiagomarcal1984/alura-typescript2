b9ff772c61192d4e89e48c0021f3744bb2a2647f
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-28 19:46:46 -0300

    Enumerations.

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,9 @@
+export enum DiasDaSemana {
+    DOMINGO, // Escrito implicitamente: DOMINGO = 0;
+    SEGUNDA, // Escrito implicitamente: SEGUNDA = 1;
+    TERCA,   // Escrito implicitamente: TERCA   = 2;
+    QUARTA,  // Escrito implicitamente: QUARTA  = 3;
+    QUINTA,  // Escrito implicitamente: QUINTA  = 4;
+    SEXTA,   // Escrito implicitamente: SEXTA   = 5;
+    SABADO   // Escrito implicitamente: SABADO  = 6;
+}
