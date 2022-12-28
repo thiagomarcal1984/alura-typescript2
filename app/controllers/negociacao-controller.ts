d71e3bfc6990196d2833a7ec93e0dfb57df37993
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-28 19:32:44 -0300

    Organizando melhor nosso código.

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -10,6 +10,8 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView("#negociacoesView");
     private mensagemView = new MensagemView("#mensagemView");
+    private readonly SABADO = 6;
+    private readonly DOMINGO = 0;
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -20,14 +22,20 @@ export class NegociacaoController {
 
     public adiciona(): void {
         const negociacao = this.criaNegociacao();
-        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) {
-            this.negociacoes.adiciona(negociacao);
-            this.atualizaView();
-            this.limparFormulario();
-        } else {
+
+        if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView
-                .update("Apenas negociações em dias úteis são aceitas.")
+                .update("Apenas negociações em dias úteis são aceitas.");
+            return; // Early return, para evitar ifs aninhados.
         }
+
+        this.negociacoes.adiciona(negociacao);
+        this.atualizaView();
+        this.limparFormulario();
+    }
+
+    private ehDiaUtil(data: Date): boolean {
+        return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO;
     }
 
     private criaNegociacao(): Negociacao {
