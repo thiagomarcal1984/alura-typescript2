1860ba388af47f309763b014a8d1f2de2aa53fa7
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-28 15:16:01 -0300

    Unindo view e modelo.

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -13,14 +13,13 @@ export class NegociacaoController {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
-        this.negociacoesView.update(); // Renderiza a view.
+        this.negociacoesView.update(this.negociacoes); // Renderiza a view.
     }
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
-        negociacao.data.setDate(12);
         this.negociacoes.adiciona(negociacao);
-        console.log(this.negociacoes.lista());
+        this.negociacoesView.update(this.negociacoes);
         this.limparFormulario();
     }
 
